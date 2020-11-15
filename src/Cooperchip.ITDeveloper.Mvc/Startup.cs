df5e5fb0bad29eb6fcfcd898f2fdff1628856228
tpincer <47256731+tpincer@users.noreply.github.com>
2020-11-15 07:33:57 -0300

    Initial commit

---
+++ src/Cooperchip.ITDeveloper.Mvc/Startup.cs
@@ -0,0 +1,62 @@
+﻿using Cooperchip.ITDeveloper.Data.ORM;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Cooperchip.ITDeveloper.Mvc
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.Configure<CookiePolicyOptions>(options =>
+            {
+                options.CheckConsentNeeded = context => true;
+                options.MinimumSameSitePolicy = SameSiteMode.None;
+            });
+
+            services.AddDbContext<ITDeveloperDbContext>(options => 
+                                    options.UseSqlServer(Configuration.GetConnectionString("DefaultITDeveloper")));
+
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+        }
+
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+            app.UseCookiePolicy();
+
+            app.UseMvc(routes =>
+            {
+                //routes.MapRoute("modulos","Prontuario/{controller=Home}/{action=Index}/{id?}");
+                //routes.MapRoute("pacientes","{controller=Home}/{action=Index}/{id}/{paciente}");
+
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller=Home}/{action=Index}/{id?}");
+            });
+        }
+    }
+}
